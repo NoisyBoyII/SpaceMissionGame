72fabb44c0c2f60e48244319e9fd8f6387a621e8
Pratap Singh <prataps@mindfiresolutions.com>
2019-10-16 15:31:47 +0530

    Added menu and score

---
+++ Snake/NewGameController.swift
@@ -0,0 +1,30 @@
+//
+//  NewGameController.swift
+//  Snake
+//
+//  Created by Pratap Singh on 16/10/19.
+//  Copyright © 2019 Pratap Singh. All rights reserved.
+//
+
+import UIKit
+
+class NewGameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
